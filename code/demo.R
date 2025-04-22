49f1a98d32b670059554b5f9edbb1e80de7b2791
JGuan-lab <64457671+JGuan-lab@users.noreply.github.com>
2025-04-23 00:57:38 +0800

    Update demo.R

--- code/demo.R
+++ code/demo.R
@@ -5,4 +5,4 @@ data_sc <- as.matrix(readRDS(paste0(path,'data_sc.rds'))
 data_bulk <- as.matrix(readRDS(paste0(path,'data_bulk.rds'))
 ###########Run scTsI#######
 source('scTsI.R')
-result <- scTsI(data_sc,theshold=0,data_bulk,k1=25,k2=25)
+result <- scTsI(data_sc,threshold=0,data_bulk,k1=25,k2=25)
